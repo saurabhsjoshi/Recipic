e30955d450b6480028bff8edac4d587f23e393e0
Akshit Tripathi <Akshit Tripathi>
2016-08-07 17:05:04 -0400

    Removed sample data from index

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,4 @@
 $( document ).ready(function() {
-    for(var i = 0; i < 10; i++) {
-        $('.content').append('<div class="card"><img src="http://www.mnftiu.cc/wp-content/uploads/2013/12/spaghetti-with-tomato-sauce.jpg" alt="Pasta Image"><p>Fucking pasta</p></div>');    
-    }
-
     $('.card').click(function() {
         console.log("click recorded");
     });
