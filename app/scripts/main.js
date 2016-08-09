1b2dd79d2a8d89b5c6b96da452ec0523ca702971
Akshit <tripathisoft@gmail.com>
2016-08-09 17:58:35 -0400

    Modified Log In flow

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
-    $('.card').click(function() {
-        console.log("click recorded");
+    $('.overlay .login .prompt .text-link').click(function() {
+        $('.hiddenLoginInput').removeClass('display', 'block');
+        $('.overlay.login.prompt').html('Already have an account? <span class="text-link" onclick="showLogInForm()">Log In!</span>');
     });
 });
