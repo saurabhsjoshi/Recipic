1118f3953ec9636a3bacf3020c19ed5940eac9ef
Saurabh Joshi <battlex2010@yahoo.com>
2016-08-12 00:33:46 -0400

    Added dynamic loading of cards when scrolling

--- app/index.php
+++ app/index.php
@@ -64,26 +64,6 @@ ZZEOF;
 			</ul>
 		</nav>
 		<div class="content">
-            <div class="card"><img src="http://mikes-table.themulligans.org/wp-content/uploads/2009/01/potato_ricotta_gnocchi-7.jpg" alt="Potato Ricotta Gnocchi"><span>Potato Ricotta Gnocchi</span></div>
-            <div class="card"><img src="https://sophnstuff.files.wordpress.com/2013/01/dsc_0086-copy1.jpg" alt="Tomato Ravioli"><span>Tomato Ravioli</span></div>
-            <div class="card"><img src="http://www.ingredientsnetwork.com/47/pdcnewsitem/03/77/22/3.jpg" alt="Rotini"><span>Rotini Pasta</span></div>
-            <div class="card"><img src="http://viafratelli.com/wp-content/uploads/2015/02/crabmeat-pasta.jpg" alt="Crabmeat Spaghetti"><span>Crabmeat Spaghetti</span></div>
-            <div class="card"><img src="http://mikes-table.themulligans.org/wp-content/uploads/2009/01/potato_ricotta_gnocchi-7.jpg" alt="Potato Ricotta Gnocchi"><span>Potato Ricotta Gnocchi</span></div>
-            <div class="card"><img src="https://sophnstuff.files.wordpress.com/2013/01/dsc_0086-copy1.jpg" alt="Tomato Ravioli"><span>Tomato Ravioli</span></div>
-            <div class="card"><img src="http://www.ingredientsnetwork.com/47/pdcnewsitem/03/77/22/3.jpg" alt="Rotini"><span>Rotini Pasta</span></div>
-            <div class="card"><img src="http://viafratelli.com/wp-content/uploads/2015/02/crabmeat-pasta.jpg" alt="Crabmeat Spaghetti"><span>Crabmeat Spaghetti</span></div>
-            <div class="card"><img src="http://mikes-table.themulligans.org/wp-content/uploads/2009/01/potato_ricotta_gnocchi-7.jpg" alt="Potato Ricotta Gnocchi"><span>Potato Ricotta Gnocchi</span></div>
-            <div class="card"><img src="https://sophnstuff.files.wordpress.com/2013/01/dsc_0086-copy1.jpg" alt="Tomato Ravioli"><span>Tomato Ravioli</span></div>
-            <div class="card"><img src="http://www.ingredientsnetwork.com/47/pdcnewsitem/03/77/22/3.jpg" alt="Rotini"><span>Rotini Pasta</span></div>
-            <div class="card"><img src="http://viafratelli.com/wp-content/uploads/2015/02/crabmeat-pasta.jpg" alt="Crabmeat Spaghetti"><span>Crabmeat Spaghetti</span></div>
-            <div class="card"><img src="http://mikes-table.themulligans.org/wp-content/uploads/2009/01/potato_ricotta_gnocchi-7.jpg" alt="Potato Ricotta Gnocchi"><span>Potato Ricotta Gnocchi</span></div>
-            <div class="card"><img src="https://sophnstuff.files.wordpress.com/2013/01/dsc_0086-copy1.jpg" alt="Tomato Ravioli"><span>Tomato Ravioli</span></div>
-            <div class="card"><img src="http://www.ingredientsnetwork.com/47/pdcnewsitem/03/77/22/3.jpg" alt="Rotini"><span>Rotini Pasta</span></div>
-            <div class="card"><img src="http://viafratelli.com/wp-content/uploads/2015/02/crabmeat-pasta.jpg" alt="Crabmeat Spaghetti"><span>Crabmeat Spaghetti</span></div>
-            <div class="card"><img src="http://mikes-table.themulligans.org/wp-content/uploads/2009/01/potato_ricotta_gnocchi-7.jpg" alt="Potato Ricotta Gnocchi"><span>Potato Ricotta Gnocchi</span></div>
-            <div class="card"><img src="https://sophnstuff.files.wordpress.com/2013/01/dsc_0086-copy1.jpg" alt="Tomato Ravioli"><span>Tomato Ravioli</span></div>
-            <div class="card"><img src="http://www.ingredientsnetwork.com/47/pdcnewsitem/03/77/22/3.jpg" alt="Rotini"><span>Rotini Pasta</span></div>
-            <div class="card"><img src="http://viafratelli.com/wp-content/uploads/2015/02/crabmeat-pasta.jpg" alt="Crabmeat Spaghetti"><span>Crabmeat Spaghetti</span></div>
 		</div>
 	</div>
 
